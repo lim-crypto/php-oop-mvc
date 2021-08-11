a63fb8c5a7140f3b232c54081f233a4a3a0cdc12
lim <wamidulim@gmail.com>
2021-08-11 22:12:29 +0800

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,28 @@
+<?php
+//database params
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','phpmvc');
+
+
+//APPROOT
+define('APPROOT' , dirname(dirname(__FILE__)));
+
+// URLROOT (dynamic link)
+define('URLROOT', 'localhost/codes/phpmvc');
+
+//sitename
+
+define('SITENAME', 'MVC Framework');
+
+
+
+
+
+
+
+
+
+
+///
---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php
+    foreach ($data['users'] as $user) {
+        echo "information " . $user->username . ' '. $user->useremail . '<br>';
+    }
